4e4a979f2bb1332d9da8388a9f7e064018d13561
EC2 Default User <ec2-user@ip-172-31-33-146.ap-northeast-1.compute.internal>
2025-05-17 11:14:56 +0000

    3-16-17day-2015

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,4 +1,4 @@
 class Book < ApplicationRecord
-  valedates :title,presence:true
-  valedates :body,presence:true
+  validates :title,presence:true
+  validates :body,presence:true
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
-  get 'homes/top'
+  # get 'homes/top' ←rootで不要になった
   resources :posts
+  root to: "homes#top"
 end
